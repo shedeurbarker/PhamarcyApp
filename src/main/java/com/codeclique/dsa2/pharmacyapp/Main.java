3cd1c5b3fe9290ae6da960463ebd0e98453d4125
David <dkeyifa@st.ug.edu.gh>
2024-07-01 23:37:10 +0000

    Update

--- src/main/java/com/codeclique/dsa2/pharmacyapp/Main.java
+++ src/main/java/com/codeclique/dsa2/pharmacyapp/Main.java
@@ -1,15 +1,16 @@
 package com.codeclique.dsa2.pharmacyapp;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 // entry level
 public class Main extends Application {
+
     @Override
     public void start(Stage primaryStage) throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
@@ -18,6 +19,7 @@ public class Main extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
+
     public static void main(String[] args) {
         launch();
     }
